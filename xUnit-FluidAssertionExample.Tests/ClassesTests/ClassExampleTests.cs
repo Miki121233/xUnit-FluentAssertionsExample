a6040ddaaed78f437a6959b1eb9fa0cb9a17b071
Mikołaj Pląder <miki.plader@gmail.com>
2023-02-25 17:03:02 +0100

    Added DateTime, Object and IEnumerable testing example

--- xUnit-FluidAssertionExample.Tests/ClassesTests/ClassExampleTests.cs
+++ xUnit-FluidAssertionExample.Tests/ClassesTests/ClassExampleTests.cs
@@ -1,23 +1,26 @@
 ﻿using FluentAssertions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using FluentAssertions.Extensions;
+using System.Net.NetworkInformation;
 using xUnit_FluidAssertionExample.Classes;
 
 namespace xUnit_FluidAssertionExample.Tests.ClassesTests
 {
     public class ClassExampleTests
     {
+        private readonly ClassExample _classExample;
+        public ClassExampleTests()
+        {
+            _classExample= new ClassExample();
+        }
+
         [Fact]
-        public void Klasa_returnFoxIfZero_ReturnString()
+        public void ClassExample_returnFoxIfZero_ReturnString()
         {
             //Arrange
-            var classExample = new ClassExample();
+            //var classExample = new ClassExample();
 
             //Act
-            var result = classExample.returnFoxIfZero(0);
+            var result = _classExample.returnFoxIfZero(0);
 
             //Assert
             result.Should().Be("Fox");
@@ -27,17 +30,54 @@ namespace xUnit_FluidAssertionExample.Tests.ClassesTests
         [InlineData(4, 1)]
         [InlineData(2, 1)]
         [InlineData(3, 1)]
-        public void Klasa_wynik_ReturnInt(int a, int b)
+        public void ClassExample_wynik_ReturnInt(int a, int b)
         {
-            //
-            var classExample = new ClassExample();
+            //Arrange
 
-            //
-            var result = classExample.sumResult(a, b);
-
-            //
+            //Act
+            var result = _classExample.sumResult(a, b);
+            //Assert
             result.Should().BeGreaterThan(2);
+        }
 
+        [Fact]
+        public void ClassExample_LastPingDate_ReturnDate()
+        {
+            var result = _classExample.LastPingDate();
+
+            result.Should().BeAfter(1.January(2010));
+            result.Should().BeBefore(1.January(2030));
+        }
+
+        [Fact]
+        public void ClassExample_GetPingOptions_ReturnObject()
+        {
+            var expected = new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+
+            var result = _classExample.GetPingOptions();
+
+            result.Should().BeOfType<PingOptions>();
+            result.Should().BeEquivalentTo(expected);
+            result.Ttl.Should().Be(1);
+        }
+
+        [Fact]
+        public void ClassExample_MostRecentPings_ReturnsIEnumerable()
+        {
+            var expected = new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+
+            var result = _classExample.MostRecentPings();
+
+            result.Should().ContainEquivalentOf(expected);
+            result.Should().Contain(x => x.DontFragment == true);
         }
     }
 }
--- xUnit-FluidAssertionExample/Classes/ClassExample.cs
+++ xUnit-FluidAssertionExample/Classes/ClassExample.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,6 +21,43 @@ namespace xUnit_FluidAssertionExample.Classes
                 return "Fox";
             }
             else { return "Not a Fox"; }
+        }
+
+        public DateTime LastPingDate()
+        {
+            return DateTime.Now;
+        }
+
+        public PingOptions GetPingOptions()
+        {
+            return new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1,
+            };
+        }
+
+        public IEnumerable<PingOptions> MostRecentPings()
+        {
+            IEnumerable<PingOptions> pingOptions = new[]
+            {
+                new PingOptions()
+                {
+                    DontFragment= true,
+                    Ttl = 1
+                },
+                new PingOptions()
+                {
+                    DontFragment= true,
+                    Ttl = 1
+                },
+                new PingOptions()
+                {
+                    DontFragment= true,
+                    Ttl = 1
+                }
+            };
+            return pingOptions;
         }
     }
 }
